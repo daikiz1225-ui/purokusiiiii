df7e05a9d2dbf11fa7339fd8c943149e6e0af8ef
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-26 14:26:59 +0900

    Delete public directory

--- public/config.js
+++
@@ -1,5 +0,0 @@
-window.__DIP_CONFIG__ = {
-    prefix: '/api/bare?url=',
-    encodeUrl: (u) => btoa(unescape(encodeURIComponent(u))).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, ''),
-    decodeUrl: (u) => decodeURIComponent(escape(atob(u.replace(/-/g, '+').replace(/_/g, '/'))))
-};
