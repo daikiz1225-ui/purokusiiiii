b786503f43c0966767efb0c5bf4f0004c070d3ef
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-26 14:34:28 +0900

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,10 @@
 self.addEventListener('fetch', e => {
     if (e.request.url.includes(location.origin) || e.request.url.startsWith('data:')) return;
     const encodeUrl = (u) => btoa(unescape(encodeURIComponent(u))).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
-    e.respondWith(fetch('/api/bare?url=' + encodeUrl(e.request.url)));
+    // 404時などのフォールバック処理
+    e.respondWith(
+        fetch('/api/bare?url=' + encodeUrl(e.request.url)).catch(() => {
+            return new Response('Retry manually', { status: 404 });
+        })
+    );
 });
