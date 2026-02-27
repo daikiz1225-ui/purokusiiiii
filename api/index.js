d17b21059b3411ea472a031e2a94ff8fa93b8924
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-27 11:45:26 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,13 +1,15 @@
 const http = require('http');
 const { createBareServer } = require('@tomphttp/bare-server-node');
+
 const bare = createBareServer('/bare/');
-const server = http.createServer();
-server.on('request', (req, res) => {
-    if (bare.shouldRoute(req)) bare.routeRequest(req, res);
-    else { res.writeHead(200); res.end('active'); }
-});
-server.on('upgrade', (req, socket, head) => {
-    if (bare.shouldRoute(req)) bare.routeUpgrade(req, socket, head);
-    else socket.end();
-});
-module.exports = server;
+
+module.exports = (req, res) => {
+  // Bare Serverへのリクエストかどうか判定
+  if (bare.shouldRoute(req)) {
+    bare.routeRequest(req, res);
+  } else {
+    // もし間違えてここに来た場合は、とりあえずステータス200を返して404を回避
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Bare Server is running');
+  }
+};
