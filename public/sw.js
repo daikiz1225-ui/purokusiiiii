653a5a8503c78b5f0d54e5f38fda39fa7ae3cb95
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-27 11:14:30 +0900

    Delete public directory

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@latest/dist/uv.bundle.js');
-importScripts('./uv.config.js');
-importScripts('https://cdn.jsdelivr.net/npm/@titaniumnetwork-dev/ultraviolet@latest/dist/uv.sw.js');
-
-const uv = new UVServiceWorker();
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(uv.fetch(event));
-});
