e5036e5f0a4c729c56b0dc25bb5b7e37a0ab9001
daikiz1225-ui <daikiz1225@icloud.com>
2026-02-27 11:16:17 +0900

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+importScripts('/lib.js');
+importScripts('/config.js');
+importScripts('/sw-core.js');
+
+const uv = new UVServiceWorker();
+self.addEventListener('fetch', event => {
+    event.respondWith(uv.fetch(event));
+});
